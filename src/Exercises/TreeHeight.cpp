080b00f1eba2526ec393a7eeda5abe6f9281808b
intcooper <107645954+intcooper@users.noreply.github.com>
2023-04-21 10:28:05 +0200

    Exercise

---
+++ src/Exercises/TreeHeight.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file TreeHeight.cpp
+ * @brief Compute the height of a binary tree.
+ *
+ * https://app.codility.com/programmers/trainings/4/tree_height/
+ */
+
+#include <algorithm>
+
+struct tree {
+  int x;
+  tree *l;
+  tree *r;
+};
+
+int treeheight(tree *T) {
+  if (T == nullptr) {
+    return 0;
+  }
+
+  int height_left = treeheight(T->l) + 1;
+  int height_right = treeheight(T->r) + 1;
+
+  return std::max(height_left, height_right);
+}
+
+int solution(tree *T) { return treeheight(T) - 1; }
